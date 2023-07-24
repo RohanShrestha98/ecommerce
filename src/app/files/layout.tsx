0d465977d39f04dcc690ad044d4fc248e012e6cf
RohanShrestha98 <nahorshrestha@gmail.com>
2023-07-24 20:07:14 +0545

    starter of ecomerce website

---
+++ src/app/files/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FilePage from './page';
+
+export default function FileLayout() {
+    return (
+        <div>
+            File LayOut
+            <FilePage />
+        </div>
+    );
+}
---
+++ src/app/pages/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PagePage from './page';
+
+export default function Layout() {
+    return (
+        <div>
+            Pages Layout
+            <PagePage />
+        </div>
+    );
+}
